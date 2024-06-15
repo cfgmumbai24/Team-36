62265226e9f62a25992a2f1c0cb752eec283884d
Piyush Bhatnagar <pbpiyush09@gmail.com>
2024-06-16 04:57:47 +0530

    added sendProductHistory function for cluster admin

--- backend/routes/clusterAdmin.js
+++ backend/routes/clusterAdmin.js
@@ -3,5 +3,5 @@ const router=express.Router()
 const clusterController=require("../controllers/clusterController")
 
 router.post('/addProduct', clusterController.addProduct);
-
+router.get('/getProducts', clusterController.getProducts);
 module.exports=router;
