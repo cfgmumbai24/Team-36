859fa8fc9d9b74eb008abf769d9bf86daf2f55f4
Sai Kaushik Sadu <91894475+SaiKaushikSadu@users.noreply.github.com>
2024-06-15 18:32:20 +0530

    routes chnages

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,7 +6,9 @@ const admin=require("../models/admin");
 router.use("/masterAdmin",require("./admin"))
 // router.use("/masterAdmin",require("./admin"))
 // router.use("/masterAdmin",require("./admin"))
-
+router.get('/', (req, res)=> {
+    res.send('heelo');
+})
 
 module.exports=router;
 
