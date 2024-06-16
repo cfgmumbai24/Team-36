0314a66e221d74354f8cf00224251e76bffe8f58
Umam Khan <umam.khan@outlook.com>
2024-06-16 05:43:55 +0530

    navbar corrected

--- frontend/src/components/navbar/index.jsx
+++ frontend/src/components/navbar/index.jsx
@@ -1,92 +1,115 @@
-import React, { useState } from "react";
-import { close, logo, menu } from "../../assets"; // Ensure these paths are correct
-import { navLinks } from "../../constants"; // Ensure this path is correct
+import React, { useState, useEffect } from "react";
+import { close, logo, menu } from "../../assets";
+import { navLinks } from "../../constants";
+import { Img } from "react-image";
 import { Link, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    // Check if user object exists in localStorage
+    const user = localStorage.getItem("user");
+    setIsLoggedIn(!!user);
+  }, []);
 
   const handleLogout = () => {
     localStorage.clear();
+    setIsLoggedIn(false);
     navigate("/login");
   };
 
   return (
-    <nav
-      className="w-full flex flex-wrap py-2 sm:py-6 justify-between items-center px-4 md:px-8 lg:px-16 xl:px-20"
-      style={{
-        background: "#ef652220",
-        boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
-        backdropFilter: "blur(50px)",
-        WebkitBackdropFilter: "blur(10px)",
-      }}
-    >
-      {/* Logo and brand name */}
-      <div className="flex items-center cursor-pointer">
-        <img src={logo} alt="logo" height={50} width={50} />
-        <p className="text-blue text-2xl font-poppins ml-2">Trustflow</p>
-      </div>
-
-      {/* Desktop navigation */}
-      <div className="hidden md:flex md:items-center md:justify-center md:gap-5">
-        {navLinks.map((nav) => (
-          <Link
-            key={nav.id}
-            to={`/${nav.id}`}
-            onClick={() => navigate(`/${nav.id}`)}
-            className="inline-block rounded-lg px-2 py-1 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100"
-          >
-            {nav.title}
-          </Link>
-        ))}
-        <button
-          onClick={handleLogout}
-          className="inline-block rounded-lg px-2 py-1 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100"
-        >
-          Logout
-        </button>
-      </div>
-
-      {/* Mobile navigation */}
-      <div className="flex items-center justify-end md:hidden">
-        {/* Hamburger menu */}
-        <img
-          src={toggle ? close : menu}
-          className="w-6 h-6 cursor-pointer"
-          alt="menu"
-          onClick={() => setToggle((prev) => !prev)}
-        />
-
-        {/* Mobile menu */}
-        {toggle && (
-          <div className="absolute top-16 right-6 w-48 bg-white rounded-lg shadow-lg z-50">
-            <ul className="flex flex-col p-4 space-y-2">
-              {navLinks.map((nav) => (
-                <li
-                  key={nav.id}
-                  className="font-medium cursor-pointer text-lg"
-                  onClick={() => {
-                    setToggle(false);
-                    navigate(`/${nav.id}`);
-                  }}
-                >
-                  <Link to={`/${nav.id}`}>{nav.title}</Link>
-                </li>
-              ))}
-              <li>
-                <button
-                  onClick={handleLogout}
-                  className="w-full text-left font-medium cursor-pointer text-lg bg-red-500 text-white py-1 px-4 rounded"
-                >
-                  Logout
-                </button>
-              </li>
-            </ul>
+    <header className="fixed inset-x-0 top-0 z-30 mx-auto w-full max-w-screen-md border border-gray-100 bg-white/80 py-3 shadow backdrop-blur-lg md:top-6 md:rounded-3xl lg:max-w-screen-lg">
+      <div className="px-4">
+        <div className="flex items-center justify-between">
+          {/* Logo and brand name */}
+          <div className="flex shrink-0 items-center cursor-pointer">
+            <Img src={logo} alt="logo" height={28} width={28} />
+            <p className="text-gray-900 text-xl font-medium ml-2">JPMMSS</p>
           </div>
-        )}
+
+          {/* Desktop navigation */}
+          <div className="hidden md:flex md:items-center md:justify-center md:gap-5">
+            {navLinks.map((nav) => (
+              <Link
+                key={nav.id}
+                to={`/${nav.id}`}
+                onClick={() => navigate(`/${nav.id}`)}
+                className="inline-block rounded-lg px-2 py-1 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100"
+              >
+                {nav.title}
+              </Link>
+            ))}
+            {isLoggedIn ? (
+              <button
+                onClick={handleLogout}
+                className="inline-block rounded-lg px-2 py-1 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100"
+              >
+                Logout
+              </button>
+            ) : (
+              <Link
+                to="/login"
+                className="inline-block rounded-lg px-2 py-1 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100"
+              >
+                Login
+              </Link>
+            )}
+          </div>
+
+          {/* Mobile navigation */}
+          <div className="flex items-center justify-end md:hidden">
+            {/* Hamburger menu */}
+            <Img
+              src={toggle ? close : menu}
+              className="w-6 h-6 cursor-pointer"
+              alt="menu"
+              onClick={() => setToggle((prev) => !prev)}
+            />
+
+            {/* Mobile menu */}
+            {toggle && (
+              <div className="absolute top-16 right-6 w-48 bg-white rounded-lg shadow-lg z-50">
+                <ul className="flex flex-col p-4 space-y-2">
+                  {navLinks.map((nav) => (
+                    <li
+                      key={nav.id}
+                      className="font-medium cursor-pointer text-lg"
+                      onClick={() => {
+                        setToggle(false);
+                        navigate(`/${nav.id}`);
+                      }}
+                    >
+                      <Link to={`/${nav.id}`}>{nav.title}</Link>
+                    </li>
+                  ))}
+                  <li>
+                    {isLoggedIn ? (
+                      <button
+                        onClick={handleLogout}
+                        className="w-full text-left font-medium cursor-pointer text-lg bg-red-500 text-white py-1 px-4 rounded"
+                      >
+                        Logout
+                      </button>
+                    ) : (
+                      <Link
+                        to="/login"
+                        className="w-full text-left font-medium cursor-pointer text-lg bg-blue-500 text-white py-1 px-4 rounded"
+                      >
+                        Login
+                      </Link>
+                    )}
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
+        </div>
       </div>
-    </nav>
+    </header>
   );
 };
 
